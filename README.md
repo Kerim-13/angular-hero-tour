ff6d959673e0a04419b4c6c8371e8564ec3b5da0
Kerim-13 <68063954+Kerim-13@users.noreply.github.com>
2022-11-08 20:29:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-hero-tour
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-tagtv4-cbaa3i)
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+export class HeroesComponent implements OnInit {
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  onSelect(hero: Hero): void {
+    if (this.selectedHero == hero) {
+      this.selectedHero = undefined;
+    } else {
+      this.selectedHero = hero;
+      this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+    }
+  }
+}
